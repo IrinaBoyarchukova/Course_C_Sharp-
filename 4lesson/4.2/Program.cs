ef74730cca34c1439d8a96bef34163513b68bc57
Ирина <bojarchukova@mail.ru>
2022-08-09 10:58:10 +0800

    4.2Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе

---
+++ 4lesson/4.2/Program.cs
@@ -0,0 +1,33 @@
+﻿/// 1. Напишите программу, которая принимает
+//    на вход число и выдаёт количество цифр в числе.
+
+// 1 вариант
+
+//int NumCount(int num)
+//{
+ //   string result = num.ToString();
+ //   return result.Length;
+//}
+
+//Console.WriteLine(NumCount(456));
+//Console.WriteLine(NumCount(78));
+//Console.WriteLine(NumCount(89126));
+
+
+// 2 вариант
+
+int NumCount(int num)
+{
+    int result = 0;
+
+    while (num > 0)
+    {
+        num /= 10;
+        result += 1;
+    }
+    return result;
+}
+
+Console.WriteLine(NumCount(456));
+Console.WriteLine(NumCount(78));
+Console.WriteLine(NumCount(89126));
