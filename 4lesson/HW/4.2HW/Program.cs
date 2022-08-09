12a090a35c5dbff30788d24a6b0c4c34a51e3db8
Ирина <bojarchukova@mail.ru>
2022-08-09 11:39:15 +0800

    4.32. Напишите программу, которая принимает  на вход число и выдаёт сумму цифр в числе.

---
+++ 4lesson/HW/4.2HW/Program.cs
@@ -0,0 +1,22 @@
+﻿// 2. Напишите программу, которая принимает  на вход число и выдаёт сумму цифр в числе.
+// https://shwanoff.ru/char-c-sharp/
+
+
+double DigitSum(int num)
+{
+    string n = num.ToString();
+    int length = n.Length;
+    double sum = 0;
+    
+    for (int i = 0; i < length; i++)
+    {   
+        double m = char.GetNumericValue(n[i]);        
+        sum += m;
+    }
+
+    return sum;
+}
+
+int n = 123;
+
+Console.WriteLine(DigitSum(n));
