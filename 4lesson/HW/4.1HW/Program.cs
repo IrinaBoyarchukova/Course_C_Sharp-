f9ccb9df61ee63ca132a0f970bfac80ff2bef6f9
Ирина <bojarchukova@mail.ru>
2022-08-09 11:31:11 +0800

    4.1HW Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.

---
+++ 4lesson/HW/4.1HW/Program.cs
@@ -0,0 +1,23 @@
+﻿// 1.Напишите цикл, который принимает на вход два числа (A и B)
+//    и возводит число A в натуральную степень B.
+
+
+int PowNum(int a, int b)
+{
+    int n_pow = a;
+
+    for (int i = 2; i <= b; i++)
+    {
+        n_pow *= a;
+    }
+    return n_pow;
+}
+
+Console.WriteLine("Enter first number: ");
+int num_1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter second number: ");
+int num_2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine(PowNum(num_1, num_2));
+
+//Console.WriteLine(PowNum(2, 4));
