56918d31fdf8e78ba1f213ed5990a182405fe4ad
Ирина <bojarchukova@mail.ru>
2022-08-17 09:28:11 +0800

    // Задайте двумерный массив размером m*n, заполненный случайными вещественными числами. // m = 3, n = 4; // 0,5 7 -2 -0,2 // 1 -3,3 8 -9,9 // 8 7,8 -7,1 9

--- 7lesson/HW/7.1HW/Program.cs
+++ 7lesson/HW/7.1HW/Program.cs
@@ -3,6 +3,7 @@
 // 0,5 7 -2 -0,2
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
+
 void Print(double[,] arr)
 {
     int row_size = arr.GetLength(0);
@@ -11,26 +12,26 @@ void Print(double[,] arr)
     for (int i = 0; i < row_size; i++)
     {
         for (int j = 0; j < column_size; j++)
-        {
-            Console.Write($" {arr[i, j]} ");
-        }
+            Console.Write($" {arr[i, j], 6} ");
         Console.WriteLine();
     }
     Console.WriteLine();
 }
-double [,] MassNums(int row, int column, int from, int to)
-{
-    double[,] arr = new double [row, column];
 
-    for(int i = 0; i < row; i++)
-    {
-        for(int j = 0; j < column; j++)
-        {
-            arr[i, j] = new Random().Next(from, to);
-        }        
-    }
-    return arr;    
+double[,] MassNums(int row, int column, int from, int to)
+{
+    double[,] arr = new double[row, column];
+    Random n_new = new Random();
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = Math.Round(n_new.NextDouble() * -10, 2);            
+    return arr;
 }
 
-double[,] arr_1 = MassNums(3, 4, -10, 11);
-Print(arr_1);     
+
+double[,] arr_1 = MassNums(3, 4, -10, 8);
+Print(arr_1);
+
+double[,] arr_2 = MassNums(5, 6, -6, 7);
+Print(arr_2);
