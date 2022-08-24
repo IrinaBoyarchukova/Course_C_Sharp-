692ad1cbfbd0bd4ae0bfcde664520c39d61ce5f7
Ирина <bojarchukova@mail.ru>
2022-08-24 17:33:56 +0800

    Дано число из отрезка [10, 99]. Показать наибольшую цифру числа

---
+++ 10lesson/10.11/Program.cs
@@ -0,0 +1,16 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+void Numbers (int a)
+{
+    if (a < 10 || a > 99)
+    Console.WriteLine("Решение невозможно");
+    else 
+    {
+        if (a % 10 > a / 10)
+        Console.WriteLine($"{a % 10}");
+        else Console.WriteLine($"{a / 10}");
+            
+    }
+}
+Console.WriteLine ("Введите 2-значное число");
+int a = int.Parse (Console.ReadLine());
+Numbers(a);
