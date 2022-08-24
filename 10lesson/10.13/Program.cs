566628043325d26afeb5322ee7bd15267f201b4f
Ирина <bojarchukova@mail.ru>
2022-08-24 17:45:08 +0800

    Выяснить, кратно ли число заданному, если нет, вывести остаток.

---
+++ 10lesson/10.13/Program.cs
@@ -0,0 +1,16 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+void Multiplicity (int a, int b)
+{
+    if (a % b == 0)
+    {
+        Console.WriteLine("Кратно");
+    }
+    else
+        Console.WriteLine($"Некратно, остаток : {a % b}");
+}
+
+Console.WriteLine ("Введите 1 число: ");
+int a = int.Parse (Console.ReadLine());
+Console.WriteLine ("Введите 2 число: ");
+int b = int.Parse (Console.ReadLine());
+Multiplicity (a, b);
